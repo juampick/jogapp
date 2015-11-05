378f5b870c0cc4dc4c89e66d25e8dcd0eda4f7e9
Juan Cook <juan.cook@globant.com>
2015-11-05 12:22:01 -0300

    initial commit with laravel+angular basic install

---
+++ app/Http/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+// ============ API Routes ============
+    Route::group(array('prefix' => 'api/v1'), function () {
+
+    });
+
+// ============ Angular Routing ============
+    Route::get('/', function () {
+        return File::get(public_path() . '/app/index.html');
+    });
+
+//If route is missing: route to angular app for routing
+    Route::any('{path?}', function () {
+        return File::get(public_path() . '/app/index.html');
+    })->where("path", ".+");
